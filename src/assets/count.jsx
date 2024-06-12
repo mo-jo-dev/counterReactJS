ce59923bc4dd72f3e4d7d30e68f6f4621599afe5
mo-jo-dev <b210055@nitsikkim.ac.in>
2024-06-12 08:43:33 +0530

    added useEffect hook

--- src/assets/count.jsx
+++ src/assets/count.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Counter(){
     const [count, setCount] = useState(0);
+    useEffect(() => {
+        document.title = `You Clicked ${count} Times`;
+        console.log(`You Clicked ${count} Tinmes`);
+    })
     return(
         <div>
+            <p>You Clicked {count} Times</p>
             <h1>Count: {count}</h1>
             <button onClick = {() => setCount(count-1)}>-</button>
             <button onClick = {() => setCount(count + 1)}>+</button>
